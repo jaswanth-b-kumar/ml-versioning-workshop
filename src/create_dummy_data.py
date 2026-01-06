771df8e69ede334ae487260d0e3bc421029d994a
jaswanth-b-kumar <124692437+jaswanth-b-kumar@users.noreply.github.com>
2026-01-06 23:48:55 +0000

    Added Iris data to DVC tracking

---
+++ src/create_dummy_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sklearn.datasets import load_iris
+
+def create_dummy_data():
+    # Load the iris dataset
+    iris = load_iris(as_frame=True)
+    
+    df = iris.frame
+    df.to_csv('data/raw/iris.csv', index=False)
+    
+if __name__ == "__main__":
+    create_dummy_data()
