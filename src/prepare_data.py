ff8e6c591b631a96fb55cc67d69b46ba99eb2ec3
jaswanth-b-kumar <124692437+jaswanth-b-kumar@users.noreply.github.com>
2026-01-10 11:01:39 +0000

    Trained Model version 1 push

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.model_selection import test_train_split
+from sklearn.model_selection import train_test_split
 from pathlib import Path
 
 RAW_DATA_PATH = Path('data/raw/iris.csv')
@@ -9,7 +9,7 @@ def main():
     PROCESSED_DATA_PATH.mkdir(parents=True, exist_ok=True)
     df = pd.read_csv(RAW_DATA_PATH)
 
-    train_df, test_df = test_train_split.train_test_split(
+    train_df, test_df = train_test_split(
         df, 
         test_size=0.2, 
         random_state=42,
